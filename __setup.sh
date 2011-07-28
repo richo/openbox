e49687c062de7ebe6627c6b130077de21f10483c
richo <richo@psych0tik.net>
2011-07-28 10:45:09 +1000

    Fix broken case

--- __setup.sh
+++ __setup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 git remote add upstream git://git.openbox.org/dana/openbox
-git remote add mikachu git://github.com/mikachu/openbox.git
+git remote add mikachu git://github.com/Mikachu/openbox.git
